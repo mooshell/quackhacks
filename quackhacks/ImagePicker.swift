8ef5bd20cbaeef10871b50b17eea1bf36849a222
Anjali Gupta <agupta08902@gmail.com>
2025-03-29 18:28:22 -0400

    maybe it works???

---
+++ quackhacks/ImagePicker.swift
@@ -0,0 +1,18 @@
+//
+//  ImagePicker.swift
+//  quackhacks
+//
+//  Created by Anjali Gupta on 3/29/25.
+//
+
+import SwiftUI
+
+struct ImagePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImagePicker()
+}
---
+++ quackhacks/camera.swift
@@ -0,0 +1,18 @@
+//
+//  camera.swift
+//  quackhacks
+//
+//  Created by Anjali Gupta on 3/29/25.
+//
+
+import SwiftUI
+
+struct camera: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    camera()
+}
