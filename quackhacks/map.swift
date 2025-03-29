a6fb0c131138a3ce37409956d67204c0dd382d06
michelle :) <michelleeliasflores@gmail.com>
2025-03-29 13:14:39 -0400

    so brazil2ndedit

--- quackhacks/map.swift
+++ quackhacks/map.swift
@@ -6,10 +6,13 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct map: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("MAP LOCATION")
+        Map()
+            .mapStyle(.hybrid)
     }
 }
 
