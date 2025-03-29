4fb2f5ee8c2b0992b826d95d1bb25db488beb238
michelle :) <michelleeliasflores@gmail.com>
2025-03-29 12:56:16 -0400

    Initial Commit

---
+++ quackhacks/quackhacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  quackhacksApp.swift
+//  quackhacks
+//
+//  Created by Michelle Elias Flores on 3/29/25.
+//
+
+import SwiftUI
+
+@main
+struct quackhacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ quackhacksUITests/quackhacksUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  quackhacksUITestsLaunchTests.swift
+//  quackhacksUITests
+//
+//  Created by Michelle Elias Flores on 3/29/25.
+//
+
+import XCTest
+
+final class quackhacksUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
